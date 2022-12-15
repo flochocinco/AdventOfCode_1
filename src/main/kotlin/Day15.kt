7876fea863a086a48c5d8aac9e8f4698640fa2ae
Florian <fverani@hotmail.com>
2022-12-16 00:14:53 +0100

    Day 15 - Part 2 not working

--- src/main/kotlin/Day15.kt
+++ src/main/kotlin/Day15.kt
@@ -1,6 +1,7 @@
 package src.main.kotlin
 
 import AdventCalendarDay
+import java.util.SortedSet
 import kotlin.math.abs
 
 class Day15 : AdventCalendarDay("input_day15.txt") {
@@ -52,6 +53,14 @@ class Day15 : AdventCalendarDay("input_day15.txt") {
     }
 
     override fun part2Impl(): Int {
+        /*val set = mutableListOf<Int>()
+        set.add(2)
+        set.add(5)
+
+        addInSet(set, 5,9)
+
+        set.forEach { println("$it,") }*/
+
         val beacons = mutableListOf<Beacon>()
         var pct = 0
         for(i in 0 until 4000000){
@@ -59,7 +68,8 @@ class Day15 : AdventCalendarDay("input_day15.txt") {
                 pct++
                 println("$pct%")
             }
-            val notInLine10 = mutableSetOf<Int>()
+            val notInLine10 = mutableListOf<Int>()
+
             inputLines.forEach {
                 val sensorX = it.split("x=")[1].split(',')[0]
                 val sensorY = it.split("y=")[1].split(':')[0]
@@ -69,29 +79,77 @@ class Day15 : AdventCalendarDay("input_day15.txt") {
                 val rayon = abs(sensorX.toInt()-beaconX.toInt()) + abs(sensorY.toInt()-beaconY.toInt())
 
                 if(sensorY.toInt()+rayon >= i){
-                    for(x in 0 until 1+rayon- abs(i-sensorY.toInt())){
-                        if(sensorX.toInt()+x <= 4000000){
-                            beacons.add(Beacon(sensorX.toInt()+x, i))
-                            notInLine10.add(sensorX.toInt()+x)
-                        }
-                        if(sensorX.toInt()-x >= 0){
-                            beacons.add(Beacon(sensorX.toInt()-x, i))
-                            notInLine10.add(sensorX.toInt()-x)
-                        }
-                    }
+                    val x= 1+rayon- abs(i-sensorY.toInt())
+                    val max = sensorX.toInt()+x.coerceAtMost(4000000)
+                    val min = sensorX.toInt()-x.coerceAtLeast(0)
+
+                    //beacons.add(Beacon(max, i))
+                    //beacons.add(Beacon(min, i))
+
+                    addInSet(notInLine10, min, max)
+
+
                 }
             }
 
-            if(notInLine10.size==4000000){
+            if(notInLine10.size == 4){
+                return i
+            }
+
+            /*if(notInLine10.size==4000000){
                 for(x in 0 until 4000000){
                     if(!notInLine10.contains(x)){
                         println("x: $x, y=$i")
                         return x*4000000+i
                     }
                 }
-            }
+            }*/
         }
 
         return -1
+    }
+
+    private fun addInSet(mutableList: MutableList<Int>, min : Int, max : Int){
+        if(mutableList.isEmpty()){
+            mutableList.add(min)
+            mutableList.add(max)
+        }
+        var idx = -1
+        for(i in 0..mutableList.size-2 step 2){
+            if(min < mutableList[i]){
+                idx = i
+                mutableList.add(i, min)
+                break
+            }
+        }
+        if(idx != -1 && idx <= mutableList.size){
+            if(mutableList[idx+1] > max){
+                mutableList.add(idx+1, max)
+            }else{
+                mutableList.removeAt(idx+1)
+            }
+        }else if(idx == -1){
+            for(i in 1 until mutableList.size step 2){
+                if(max > mutableList[i]){
+                    if(mutableList[i] > min){
+                        mutableList.removeAt(i)
+                        mutableList.add(i, max)
+                    }else{
+                        mutableList.add(min)
+                        mutableList.add(max)
+                    }
+                    break
+                }
+            }
+        }
+        //special case
+        val intToRemove = mutableListOf<Int>()
+        for(i in 1 until mutableList.size - 2){
+            if(mutableList[i+1] - mutableList[i] <= 1){
+                intToRemove.add(mutableList[i+1])
+                intToRemove.add(mutableList[i])
+            }
+        }
+        mutableList.removeAll(intToRemove)
     }
 }
