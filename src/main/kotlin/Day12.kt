1015e6b1e28280fd083a9040e749cca713a62212
Florian <fverani@hotmail.com>
2022-12-22 12:03:06 +0100

    Day 12: Part 1 done!

--- src/main/kotlin/Day12.kt
+++ src/main/kotlin/Day12.kt
@@ -10,9 +10,10 @@ private const val END_SYMBOL = 'E'
 
 class Day12 : AdventCalendarDay("input_day12.txt") {
 
-    class Edge(x: Int, y : Int) : Day14.Point(x,y) {
-        var shortestDistanceFromStart = Int.MAX_VALUE
+    class Edge(x: Int, y : Int) : Day14.Point(x,y){
+        var distance = Int.MAX_VALUE
     }
+
 
     override fun part1Impl(): Int {
         var steps = Int.MAX_VALUE
@@ -22,76 +23,56 @@ class Day12 : AdventCalendarDay("input_day12.txt") {
         inputLines.forEach {map.add(it.map { c -> getTranformedValue(c)})}
 
         val absoluteStartPosition = map.flatten().indexOf(getTranformedValue(START_SYMBOL))
-        val startPoint = Day14.Point(absoluteStartPosition%map[0].size, absoluteStartPosition/map[0].size)
+        val startPoint = Edge(absoluteStartPosition%map[0].size, absoluteStartPosition/map[0].size)
+        startPoint.distance=0
         val absoluteEndPosition = map.flatten().indexOf(getTranformedValue(END_SYMBOL))
-        val endPoint = Day14.Point(absoluteEndPosition%map[0].size, absoluteEndPosition/map[0].size)
+        val endPoint = Edge(absoluteEndPosition%map[0].size, absoluteEndPosition/map[0].size)
 
-        val path = mutableListOf<Day14.Point>()
+        val pointToDistance = mutableMapOf<Edge, Int>()
+
+        val path = mutableListOf<Edge>()
         path.add(startPoint)
 
-        val pathSize = getPath(startPoint, endPoint, path, map)
-        steps = steps.coerceAtMost(pathSize)
-        println("Shortest Path is: $pathSize to go from $startPoint to $endPoint")
+        getPath(startPoint, endPoint, path, map, pointToDistance)
 
+        //println("Shortest Path is: $pathSize to go from $startPoint to $endPoint")
 
-
-        return steps
+        return pointToDistance[endPoint]!!
     }
 
-    val leadingToVoid = mutableSetOf<Day14.Point>()
+    val leadingToVoid = mutableSetOf<Edge>()
     var counter = 0L
-    private fun getPath(startPoint : Day14.Point, endPoint: Day14.Point, path: MutableList<Day14.Point>, map : List<List<Int>>) : Int{
+    private fun getPath( startPoint: Edge, endPoint: Edge, path: MutableList<Edge>, map: List<List<Int>>, pointToDistance: MutableMap<Edge, Int>    ){
         counter++
-        println("${counter}: Current Chat value : ${map[startPoint.y][startPoint.x].toChar()} at point ${startPoint.x+1};${startPoint.y+1}")
+        println("${counter}: Current Chat value : ${map[startPoint.y][startPoint.x].toChar()} at point ${startPoint.x+1};${startPoint.y+1}, has a distance to start of: ${startPoint.distance}")
         //println("Path $path")
         var possibilities =
             listOf(getPointRight(startPoint), getPointUp(startPoint), getPointDown(startPoint), getPointLeft(startPoint))
-                .filter { !leadingToVoid.contains(it) && isLegal(startPoint, it, map) && !path.contains(it)}
-                .filter { map[it.y][it.x] >= map[startPoint.y][startPoint.x] } //climb first if possible
-                .sortedWith(comparator = { first, second ->
-                    val charValue = map[second.y][second.x].compareTo(map[first.y][first.x])
-                    if(charValue != 0){
-                        return@sortedWith charValue
-                    }
-                    val d1 = getDistance(first!!, endPoint)
-                    val d2 = getDistance(second!!, endPoint)
+                .filter { isLegal(startPoint, it, map) && !path.contains(it)}
 
-                    if(d1 != d2){
-                        return@sortedWith d1.compareTo(d2)
-                    }
-                    return@sortedWith abs(endPoint.y-first!!.y).coerceAtLeast(abs(endPoint.x-first.x))
-                        .coerceAtMost(abs(endPoint.y-second!!.y).coerceAtLeast(abs(endPoint.x-second.x)))
-                })
-                //.sortedByDescending { abs(it.x-endPoint.x).coerceAtLeast((it.y-endPoint.y))}
-                //.sortedByDescending { map[it.y][it.x] }
-                .toList()
-        //keep only cell which make us climb
-        //possibilities = possibilities.filter { map[it.y][it.x] == map[possibilities[0].y][possibilities[0].x] }
-        //now priority on shortest path
-        //possibilities.sortedBy { abs(it.x-endPoint.x) + abs(it.y-endPoint.y) }
-        /*possibilities = possibilities.filter { map[it.y][it.x] == map[possibilities[0].y][possibilities[0].x] }
-        possibilities.sortedBy { (endPoint.x-it.x) + (endPoint.y-it.y)}
-            .sortedByDescending { map[it.y][it.x] }*/
         if(possibilities.isEmpty()) {
-            return Int.MAX_VALUE
+            return
         }
         if(possibilities.contains(endPoint)){
             //path.add(endPoint) as the path contains starting point, do not check in
             println("Found path of length: ${path.size}")
-            return path.size
         }
-        leadingToVoid.addAll(possibilities)
-        val size = possibilities.minOf {
+        val currentDistance = startPoint.distance
+        possibilities.forEach {
+            //update distance
+            if(pointToDistance.contains(it) && pointToDistance[it]!! <= currentDistance+1){
+                return@forEach
+            }
+            pointToDistance[it] = currentDistance+1
+            it.distance = currentDistance+1
+            //recurse on possibilities
             path.add(it)
-            val tmp = getPath(it, endPoint, path, map)
+            getPath(it, endPoint, path, map, pointToDistance)
             path.remove(it)
-            tmp
         }
-        leadingToVoid.removeAll(possibilities.toSet())
-        return size
     }
 
-    private fun getDistance(it: Day14.Point, endPoint: Day14.Point) =
+    private fun getDistance(it: Edge, endPoint: Edge) =
         abs(it.x - endPoint.x) + abs(it.y - endPoint.y)
 
     private fun getTranformedValue(c : Char) : Int{
@@ -102,23 +83,23 @@ class Day12 : AdventCalendarDay("input_day12.txt") {
         }else{c.code }
     }
 
-    private fun getPointUp(startPoint: Day14.Point) : Day14.Point{
-        return Day14.Point(startPoint.x , startPoint.y-1)
+    private fun getPointUp(startPoint: Edge) : Edge{
+        return Edge(startPoint.x , startPoint.y-1)
     }
 
-    private fun getPointDown(startPoint: Day14.Point) : Day14.Point{
-        return Day14.Point(startPoint.x , startPoint.y+1)
+    private fun getPointDown(startPoint: Edge) : Edge{
+        return Edge(startPoint.x , startPoint.y+1)
     }
 
-    private fun getPointRight(startPoint: Day14.Point) : Day14.Point{
-        return Day14.Point(startPoint.x+1 , startPoint.y)
+    private fun getPointRight(startPoint: Edge) : Edge{
+        return Edge(startPoint.x+1 , startPoint.y)
     }
 
-    private fun getPointLeft(startPoint: Day14.Point) : Day14.Point{
-        return Day14.Point(startPoint.x-1 , startPoint.y)
+    private fun getPointLeft(startPoint: Edge) : Edge{
+        return Edge(startPoint.x-1 , startPoint.y)
     }
 
-    private fun isLegal(startPoint: Day14.Point, endPoint: Day14.Point, map : List<List<Int>>) : Boolean {
+    private fun isLegal(startPoint: Edge, endPoint: Edge, map : List<List<Int>>) : Boolean {
         return endPoint.x >= 0 && endPoint.y >= 0 && endPoint.x < map[0].size && endPoint.y < map.size
                 && map[endPoint.y][endPoint.x] - map[startPoint.y][startPoint.x] <= 1 /* at most 1 if climbing or anything if descending*/
     }
