79bf90542de489ac3f66b0b5544620a317394910
Florian <fverani@hotmail.com>
2022-12-23 11:11:37 +0100

    Day 17: Reworked code to use class for Tower

--- src/main/kotlin/Day17.kt
+++ src/main/kotlin/Day17.kt
@@ -6,65 +6,81 @@ import java.math.BigInteger
 class Day17 : AdventCalendarDay("input_day17.txt") {
     private var pieceCounter = 0
     override fun part1Impl(): Int {
-        val tower = mutableListOf<Array<Int>>()
+        val tower = Tower()
 
-        var nextShape = getNextShape(0, 3)
+        var shape = getNextShape(0, 3)
         repeat(4){
-            tower.add(createLine())
+            tower.createLine()
         }
         val numberOfInputs = inputLines[0].length
         var round = 0
         while(pieceCounter < 2022){
             val c = inputLines[0][round%numberOfInputs]
             if(c == '<'){
-                nextShape.moveLeft(tower)
+                shape.moveLeft(tower)
             }else if(c == '>'){
-                nextShape.moveRight(tower)
+                shape.moveRight(tower)
             }
-            if(!nextShape.canMoveDown(tower)){
-                addInTower(nextShape, tower)
-                nextShape = getNextShape(++pieceCounter, tower.size+3)
-                repeat(nextShape.getHeight()+3){
-                    tower.add(createLine())
+            if(!shape.canMoveDown(tower)){
+                tower.addShape(shape)
+                shape = getNextShape(++pieceCounter, tower.size()+3)
+                repeat(shape.getHeight()+3){
+                    tower.createLine()
                 }
             }else{
-                nextShape.moveDown()
-                if(tower.last().all { it == 0 }){
-                    tower.removeLast()
-                }
+                shape.moveDown()
+                tower.removeUpperLineIfEmpty()
             }
             round++
         }
 
         println("Final State")
+        tower.cleanEmptyLines()
+        tower.print()
 
-        tower.removeIf { it.all { place -> place == 0 } }
-        printTower(tower)
-
-        return tower.size
+        return tower.size()
     }
 
-    private fun createLine(): Array<Int> {
-        return Array(7){0}
-    }
-
-    private fun addInTower(shape : Shape, tower : List<Array<Int>>){
-        shape.points.forEach {
-            tower[it.y][it.x] = 1
+    class Tower {
+        val data = mutableListOf<Array<Int>>()
+        fun createLine() {
+            data.add(Array(7){0})
         }
-    }
 
-    private fun printTower(tower : List<Array<Int>>){
-        tower.reversed().forEachIndexed { idx, it ->
-            print('|')
-            it.forEach {pos ->  if(pos==0){print('.')}else{print('#') }}
-            print('|')
-            if(idx%10==0){
-                print(" ${tower.size-idx}")
+        fun size() : Int{
+            return data.size
+        }
+
+        fun addShape(shape : Shape){
+            shape.points.forEach {
+                data[it.y][it.x] = 1
             }
-            println()
         }
-        println("+-------+")
+
+        fun print(){
+            data.reversed().forEachIndexed { idx, it ->
+                print('|')
+                it.forEach {pos ->  if(pos==0){print('.')}else{print('#') }}
+                print('|')
+                if(idx%10==0){
+                    print(" ${data.size-idx}")
+                }
+                println()
+            }
+            println("+-------+")
+        }
+
+        fun cleanEmptyLines() {
+            while(data.last().all { it == 0 }){
+                data.removeLast()
+            }
+        }
+
+        fun removeUpperLineIfEmpty() {
+            if(data.last().all { it == 0 }){
+                data.removeLast()
+            }
+        }
     }
 
     override fun part2Impl(): BigInteger {
@@ -83,8 +99,8 @@ class Day17 : AdventCalendarDay("input_day17.txt") {
 
     class Shape(val points : List<Day14.Point>){
 
-        fun canMoveDown(tower: MutableList<Array<Int>>): Boolean{
-            return points.all { it.y > 0 } && points.all { tower[it.y-1][it.x] == 0 }
+        fun canMoveDown(tower: Tower): Boolean{
+            return points.all { it.y > 0 } && points.all { tower.data[it.y-1][it.x] == 0 }
         }
 
         fun getHeight() : Int{
@@ -96,18 +112,18 @@ class Day17 : AdventCalendarDay("input_day17.txt") {
                 it.y = 0.coerceAtLeast(it.y-1)
             }
         }
-        fun moveRight(tower: MutableList<Array<Int>>){
+        fun moveRight(tower: Tower){
             if(points.any { it.x == 6 }
-                || points.any { tower[it.y][it.x+1] == 1}){
+                || points.any { tower.data[it.y][it.x+1] == 1}){
                 return
             }
             points.forEach {
                 it.x+=1
             }
         }
-        fun moveLeft(tower: MutableList<Array<Int>>){
+        fun moveLeft(tower: Tower){
             if(points.any { it.x == 0 }
-                || points.any { tower[it.y][it.x-1] == 1}){
+                || points.any { tower.data[it.y][it.x-1] == 1}){
                 return
             }
             points.forEach {
